814f1288b9183e262844002e47a9dcc4667b6816
Joshua Melville <joshua.melville@northwestern.edu>
2023-03-10 16:27:58 +0200

    adding untracked

---
+++ packages/api/.eslintrc.js
@@ -0,0 +1,7 @@
+// require("@rushstack/eslint-patch/modern-module-resolution");
+
+module.exports = {
+  root: true,
+  extends: ["@codaco/eslint-config"],
+  parserOptions: { tsconfigRootDir: __dirname }
+};
