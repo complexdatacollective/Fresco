bc91d661d97c894f2a9b1ccde880a25e9f1b9aa6
Caden Buckhalt <buckhalt@northwestern.edu>
2023-05-08 12:26:04 -0700

    use role for protected api route access

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -9,6 +9,22 @@ export async function GET() {
     return NextResponse.json({ message: 'Unauthorized.' })
   }
 
+  const user = await prisma.user.findUnique({
+    where: {
+      email: session.user.email,
+    },
+    select: {
+      id: true,
+      email: true,
+      name: true,
+      roles: true,
+    }
+  });
+
+  if (user.roles && user.roles.some(e => e.name === 'PARTICIPANT')) {
+    return NextResponse.json({ message: 'Unauthorized.' })
+  }
+
   const res = await prisma.user.findMany();
 
   return NextResponse.json({ data: res });
