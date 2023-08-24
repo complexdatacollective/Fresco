99aed9aa2a684afd596fab0f5d64bc49b8b8e012
Caden Buckhalt <halaeb@gmail.com>
2023-08-24 14:57:07 -0700

    delete button enabled when rows are selected

--- components/DataTable/DataTable.tsx
+++ components/DataTable/DataTable.tsx
@@ -134,28 +134,37 @@ export function DataTable<TData, TValue>({
         </Table>
       </div>
       <div>
-        <div className="flex-1 text-sm text-muted-foreground">
-          {table.getFilteredSelectedRowModel().rows.length} of{' '}
-          {table.getFilteredRowModel().rows.length} row(s) selected.
+        <div className="flex justify-between py-4">
+          <div className="text-sm text-muted-foreground">
+            {table.getFilteredSelectedRowModel().rows.length} of{' '}
+            {table.getFilteredRowModel().rows.length} row(s) selected.
+          </div>
+          <div className="space-x-2">
+            <Button
+              variant="outline"
+              size="sm"
+              onClick={() => table.previousPage()}
+              disabled={!table.getCanPreviousPage()}
+            >
+              Previous
+            </Button>
+            <Button
+              variant="outline"
+              size="sm"
+              onClick={() => table.nextPage()}
+              disabled={!table.getCanNextPage()}
+            >
+              Next
+            </Button>
+          </div>
         </div>
-        <div className="flex items-center justify-end space-x-2 py-4">
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.previousPage()}
-            disabled={!table.getCanPreviousPage()}
-          >
-            Previous
-          </Button>
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.nextPage()}
-            disabled={!table.getCanNextPage()}
-          >
-            Next
-          </Button>
-        </div>
+        <Button
+          variant="destructive"
+          size="sm"
+          disabled={!table.getFilteredSelectedRowModel().rows.length}
+        >
+          Delete Selected
+        </Button>
       </div>
     </div>
   );
