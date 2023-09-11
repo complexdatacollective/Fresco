ee85bc5737c327e7ea5e28880b8a75b90b6cf010
Caden Buckhalt <halaeb@gmail.com>
2023-09-11 07:33:44 -0700

    remove prev implementation of setup wizard

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,11 +1,8 @@
 import { sessionGuard } from '~/utils/auth';
 import { NavigationBar } from './_components/NavigationBar';
-import getSetupMetadata from '~/utils/getSetupMetadata';
-import SetupWizard from './_components/SetupWizard/SetupWizard';
 
 async function Home() {
   await sessionGuard();
-  const setupMetadata = await getSetupMetadata();
 
   return (
     <>
@@ -15,10 +12,6 @@ async function Home() {
           <h1 className="mb-2 text-3xl font-bold">Welcome</h1>
           <p>This is the main dashboard.</p>
         </div>
-        {!setupMetadata.configured ? (
-            <SetupWizard />
-          ) :("")}
-          
       </main>
     </>
   );
