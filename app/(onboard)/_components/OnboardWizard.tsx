7329171d1ce64b7b3dc6d597ef480131ac066fa1
Caden Buckhalt <halaeb@gmail.com>
2023-09-12 07:22:48 -0700

    style form using panel style with frosted border

--- app/(onboard)/_components/OnboardWizard.tsx
+++ app/(onboard)/_components/OnboardWizard.tsx
@@ -5,6 +5,7 @@ import OnboardSteps from '~/app/(onboard)/_components/OnboardSteps/OnboardSteps'
 import CreateAccount from '~/app/(onboard)/_components/OnboardSteps/CreateAccount';
 import ConfigureStudy from '~/app/(onboard)/_components/OnboardSteps/ConfigureStudy';
 import Documentation from '~/app/(onboard)/_components/OnboardSteps/Documentation';
+import { cn } from '~/utils/shadcn';
 
 function OnboardWizard() {
   const [step, setStep] = useState(1);
@@ -13,10 +14,17 @@ function OnboardWizard() {
     setStep(step + 1);
   };
 
+  const cardClasses = cn(
+    'relative mt-[-60px] flex flex-row rounded-xl  bg-white p-8',
+    'after:absolute after:inset-[-20px] after:z-[-1] after:rounded-3xl after:bg-white/30 after:shadow-2xl after:backdrop-blur-sm',
+  );
+
+  const userFormClasses = cn('relative flex w-[30rem] flex-row p-8');
+
   return (
-    <div className="flex rounded-xl bg-white/80 p-6 shadow-lg backdrop-blur-md">
+    <div className={cardClasses}>
       <OnboardSteps currentStep={step} />
-      <div className="flex flex-col pl-12 pr-12">
+      <div className={userFormClasses}>
         {step === 1 && <CreateAccount handleNextStep={handleNextStep} />}
         {step === 2 && <ConfigureStudy handleNextStep={handleNextStep} />}
         {step === 3 && <Documentation />}
