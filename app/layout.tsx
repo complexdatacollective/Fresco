f3af5fdf81c08f43306acd2a4b6f247bee1eeb5b
Caden Buckhalt <halaeb@gmail.com>
2023-09-18 14:25:32 -0700

    cache metadata db req for perf improvement

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,15 +3,18 @@ import Providers from './_components/Providers';
 import getSetupMetadata from '~/utils/getSetupMetadata';
 import { redirect } from 'next/navigation';
 import { getPageSession } from '~/utils/auth';
+import { cache } from 'react';
 
 export const metadata = {
   title: 'Network Canvas Fresco',
   description: 'Fresco.',
 };
 
+const getSetupMetadataCached = cache(getSetupMetadata);
+
 async function RootLayout({ children }: { children: React.ReactNode }) {
   const session = await getPageSession();
-  const setupMetadata = await getSetupMetadata();
+  const setupMetadata = await getSetupMetadataCached();
 
   if (session && setupMetadata.configured) {
     console.log('onboard layout: session exists. redirecting.');
