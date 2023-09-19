b05167bee98b792f17735baae8cb12db99eff6d8
Joshua Melville <joshua.melville@northwestern.edu>
2023-09-19 13:40:26 +0200

    refactor getSetupMetadata for better readability

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,23 @@
 import '~/styles/globals.scss';
 import Providers from './_components/Providers';
-import getSetupMetadata from '~/utils/getSetupMetadata';
 import { redirect } from 'next/navigation';
 import { getPageSession } from '~/utils/auth';
-import { cache } from 'react';
+import getSetupMetadataCached from '~/utils/getSetupMetadata';
 
 export const metadata = {
   title: 'Network Canvas Fresco',
   description: 'Fresco.',
 };
 
-const getSetupMetadataCached = cache(getSetupMetadata);
-
 async function RootLayout({ children }: { children: React.ReactNode }) {
   const session = await getPageSession();
-  const setupMetadata = await getSetupMetadataCached();
+  const { configured, expired } = await getSetupMetadataCached();
 
-  if (session && setupMetadata.configured) {
-    console.log('onboard layout: session exists. redirecting.');
+  if (session && configured) {
     redirect('/dashboard');
   }
 
-  const configExpired: boolean =
-    Date.now() - setupMetadata.initializedAt.getTime() > 300000;
-  if (!setupMetadata.configured && configExpired) {
+  if (!configured && expired) {
     redirect('/?step=expired');
   }
   return (
--- utils/getSetupMetadata.ts
+++ utils/getSetupMetadata.ts
@@ -1,8 +1,14 @@
 /* eslint-disable local-rules/require-data-mapper */
 import type { SetupMetadata } from '@prisma/client';
+import { cache } from 'react';
+import { UNCONFIGURED_TIMEOUT } from '~/fresco.config';
 import { prisma } from '~/utils/db';
 
-export default async function getSetupMetadata(): Promise<SetupMetadata> {
+type SetupMetadataWithExpired = SetupMetadata & {
+  expired: boolean;
+};
+
+async function getSetupMetadata(): Promise<SetupMetadataWithExpired> {
   let setupMetadata = await prisma.setupMetadata.findFirst();
 
   // if no setup metadata exists, seed it
@@ -15,5 +21,13 @@ export default async function getSetupMetadata(): Promise<SetupMetadata> {
     });
   }
 
-  return setupMetadata;
+  return {
+    ...setupMetadata,
+    expired:
+      setupMetadata.initializedAt.getTime() < Date.now() - UNCONFIGURED_TIMEOUT,
+  };
 }
+
+const getSetupMetadataCached = cache(getSetupMetadata);
+
+export default getSetupMetadataCached;
