5593c07cdacaecdc4542f3493eb4d725e65f2bc8
Joshua Melville <joshua.melville@northwestern.edu>
2023-09-29 16:04:36 +0200

    remove withUrlHeader middleware as it is no longer used

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,8 @@
 import { stackMiddlewares } from './middlewares/stackMiddleware';
 import { withErrorHandler } from './middlewares/withErrorHandler';
 import { withLogger } from './middlewares/withLogger';
-import { withURLHeader } from './middlewares/withURLHeader';
 
-export default stackMiddlewares([withURLHeader, withLogger, withErrorHandler]);
+export default stackMiddlewares([withLogger, withErrorHandler]);
 
 export const config = {
   matcher: ['/((?!api|_next/static|images|_next/image|favicon.ico).*)'],
