22b15b1a87d322161396a5f77da655009b7056a9
Joshua Melville <joshua.melville@northwestern.edu>
2023-09-29 11:16:15 +0200

    implement RedirectWrapper

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,9 @@
 /* eslint-disable no-console */
 import '~/styles/globals.scss';
 import Providers from '../providers/Providers';
-import { headers } from 'next/headers';
 import { getSetupMetadata } from '~/utils/getSetupMetadata';
-import { calculateRedirect } from '~/utils/calculateRedirectedRoutes';
-import { api } from './_trpc/server';
 import { caller } from './_trpc/caller';
+import RedirectWrapper from '~/components/RedirectWrapper';
 
 export const metadata = {
   title: 'Network Canvas Fresco',
@@ -13,20 +11,21 @@ export const metadata = {
 };
 
 async function RootLayout({ children }: { children: React.ReactNode }) {
-  // const session = await api.session.get.query();
   const session = await caller.session.get();
   const { expired, configured } = await getSetupMetadata();
-  const headersList = headers();
-  const path = headersList.get('x-url');
 
   console.log('rootlayout', session);
-
-  calculateRedirect({ session, path, expired, configured });
 
   return (
     <html lang="en">
       <body>
-        <Providers initialSession={session}>{children}</Providers>
+        <RedirectWrapper
+          configured={configured}
+          expired={expired}
+          session={session}
+        >
+          <Providers initialSession={session}>{children}</Providers>
+        </RedirectWrapper>
       </body>
     </html>
   );
---
+++ components/RedirectWrapper.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import type { Session } from 'lucia';
+import { usePathname, useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+import { calculateRedirect } from '~/utils/calculateRedirectedRoutes';
+
+/**
+ *
+ * This wrapper component determines if we need to redirect based on if the
+ * user is logged in, if the app is configured, and if the confiiuration window
+ * is expired.
+ *
+ * Initially implemented within the root layout, but this caused maximum update
+ * depth exceeded errors for unknown reasons.
+ *
+ * Logic for redirection is in utils/calculateRedirectedRoutes.ts
+ */
+
+export default function RedirectWrapper({
+  session,
+  children,
+  configured,
+  expired,
+}: {
+  session: Session | null;
+  children: React.ReactNode;
+  configured: boolean;
+  expired: boolean;
+}) {
+  const router = useRouter();
+  const path = usePathname();
+
+  useEffect(() => {
+    const redirect = calculateRedirect({
+      session,
+      path,
+      expired,
+      configured,
+    });
+
+    console.log('redirect', redirect, session);
+
+    if (redirect) {
+      router.push(redirect);
+    }
+  }, [session, path, router, configured, expired]);
+
+  return children;
+}
