7c36bebfbbe09fa311b347479c8c01ade6b628e1
Joshua Melville <joshua.melville@northwestern.edu>
2023-09-29 16:57:26 +0200

    refactor participant router

--- server/router.ts
+++ server/router.ts
@@ -2,7 +2,7 @@ import { metadataRouter } from './routers/metadata';
 import { sessionRouter } from './routers/session';
 import { interviewRouter } from './routers/interview';
 import { protocolRouter } from '~/server/routers/protocol';
-import { participantRouter } from './routers/participants';
+import { participantRouter } from './routers/participant';
 import { router } from './trpc';
 
 export const appRouter = router({
