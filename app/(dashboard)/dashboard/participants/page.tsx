9b8257a843ed244cc248db82c0d15b412fa2c52e
Joshua Melville <joshua.melville@northwestern.edu>
2023-10-10 15:26:05 +0200

    add revalidate:0 to participants dashboard route

--- app/(dashboard)/dashboard/participants/page.tsx
+++ app/(dashboard)/dashboard/participants/page.tsx
@@ -2,7 +2,11 @@ import { ParticipantsTable } from '~/app/(dashboard)/dashboard/_components/Parti
 import { trpc } from '~/app/_trpc/server';
 
 const ParticipantPage = async () => {
-  const participants = await trpc.participant.get.all.query();
+  const participants = await trpc.participant.get.all.query(undefined, {
+    context: {
+      revalidate: 0,
+    },
+  });
   return (
     <div className="rounded-lg bg-white p-6">
       <h2 className="mb-6 text-2xl font-bold">Participant management view</h2>
