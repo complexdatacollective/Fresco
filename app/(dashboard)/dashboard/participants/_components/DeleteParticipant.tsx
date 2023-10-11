8b6d948746dabf637cd5c03eaa4f9861f198facb
Caden Buckhalt <halaeb@gmail.com>
2023-10-11 12:56:52 -0700

    style warnings for interview data

--- app/(dashboard)/dashboard/participants/_components/DeleteParticipant.tsx
+++ app/(dashboard)/dashboard/participants/_components/DeleteParticipant.tsx
@@ -1,5 +1,5 @@
 import { type Participant } from '@prisma/client';
-import { Loader2 } from 'lucide-react';
+import { Loader2, AlertCircle, Trash2 } from 'lucide-react';
 import { Button } from '~/components/ui/Button';
 import {
   AlertDialog,
@@ -10,6 +10,8 @@ import {
   AlertDialogHeader,
   AlertDialogTitle,
 } from '~/components/ui/alert-dialog';
+
+import { Alert, AlertDescription, AlertTitle } from '~/components/ui/Alert';
 
 // Check if participant has interviews
 // Check if participant interviews have been exported using exportTime
@@ -43,31 +45,41 @@ export const DeleteParticipant = ({
     <AlertDialog open={open} onOpenChange={onCancel}>
       <AlertDialogContent>
         <AlertDialogHeader>
-          <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
           <AlertDialogDescription>
             This action cannot be undone. This will permanently delete{' '}
             <strong>{selectedParticipants.length} participant(s).</strong>
           </AlertDialogDescription>
-          {hasInterviews && (
-            <div>
-              <strong>Warning:</strong> One or more of the selected participants
-              has interview data that will also be deleted.
-            </div>
+          {hasInterviews && !hasInterviewsNotYetExported && (
+            <Alert>
+              <AlertCircle className="h-4 w-4" />
+              <AlertTitle>Warning</AlertTitle>
+              <AlertDescription>
+                One or more of the selected participants has interview data that
+                will also be deleted.
+              </AlertDescription>
+            </Alert>
           )}
           {hasInterviewsNotYetExported && (
-            <div>
-              <strong>Warning:</strong> This interview data has not yet been
-              exported.
-            </div>
+            <Alert variant="destructive">
+              <AlertCircle className="h-4 w-4" />
+              <AlertTitle>Warning</AlertTitle>
+              <AlertDescription>
+                One or more of the selected participants has{' '}
+                <strong> unexported </strong>
+                interview data that will also be deleted.
+              </AlertDescription>
+            </Alert>
           )}
         </AlertDialogHeader>
         <AlertDialogFooter>
           <AlertDialogCancel disabled={isLoading} onClick={onCancel}>
             Cancel
           </AlertDialogCancel>
-          <Button onClick={onConfirm}>
+          <Button onClick={onConfirm} variant="destructive">
+            <Trash2 className="mr-2 h-4 w-4" />
             {isLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
-            {isLoading ? 'Deleting...' : 'Continue'}
+            {isLoading ? 'Deleting...' : 'Permanently Delete'}
           </Button>
         </AlertDialogFooter>
       </AlertDialogContent>
