11441d7d6b969ba9c0252748bc8ef818cdd3dbba
Caden Buckhalt <halaeb@gmail.com>
2023-10-19 11:15:19 -0700

    404 pages

--- app/(interview)/interview/not-found.tsx
+++ app/(interview)/interview/not-found.tsx
@@ -1,13 +1,27 @@
-import Link from 'next/link';
+'use client';
+
+import { FileWarning } from 'lucide-react';
+import { Button } from '~/components/ui/Button';
+import { useRouter } from 'next/navigation';
 
 export default function InterviewNotFound() {
+  const router = useRouter();
+  const redirectToDashboard = () => {
+    router.push('/');
+  };
+
   return (
-    <div className="min-h-screen">
-      <h1>404 - Interview Not Found</h1>
-      <p> No interview with the provided id was found</p>
-      <p>
-        Go back to the <Link href="/">Dashboard</Link>
-      </p>
+    <div className="flex h-screen flex-col items-center justify-center bg-gray-100">
+      <FileWarning className="mb-4 h-12 w-12 text-violet-600" />
+      <h1 className="text-3xl font-extrabold text-violet-700">404</h1>
+      <p className="text-lg text-gray-700">Interview not found</p>
+      <Button
+        variant="outline"
+        className="mt-4 bg-violet-600 text-white"
+        onClick={redirectToDashboard}
+      >
+        Back to Dashboard
+      </Button>
     </div>
   );
 }
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,12 +1,26 @@
-import Link from 'next/link';
+'use client';
+
+import { FileWarning } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import { Button } from '~/components/ui/Button';
 
 export default function NotFound() {
+  const router = useRouter();
+  const redirectToDashboard = () => {
+    router.push('/');
+  };
   return (
-    <div>
-      <h1>404 - Page Not Found</h1>
-      <p>
-        Go back to the <Link href="/">Dashboard</Link>
-      </p>
+    <div className="flex h-screen flex-col items-center justify-center bg-gray-100">
+      <FileWarning className="mb-4 h-12 w-12 text-violet-600" />
+      <h1 className="text-3xl font-extrabold text-violet-700">404</h1>
+      <p className="text-lg text-gray-700">Page not found</p>
+      <Button
+        variant="outline"
+        className="mt-4 bg-violet-600 text-white hover:bg-violet-700"
+        onClick={redirectToDashboard}
+      >
+        Back to Dashboard
+      </Button>
     </div>
   );
 }
