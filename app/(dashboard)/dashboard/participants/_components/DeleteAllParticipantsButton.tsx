d6490d619f416a5fbaf6807fd1556815af543558
Joshua Melville <joshua.melville@northwestern.edu>
2023-10-23 13:02:14 +0200

    fix ts error in interviews table by inferring convertedData type

--- app/(dashboard)/dashboard/participants/_components/DeleteAllParticipantsButton.tsx
+++ app/(dashboard)/dashboard/participants/_components/DeleteAllParticipantsButton.tsx
@@ -12,7 +12,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
-} from '~/components/ui/alert-dialog';
+} from '~/components/ui/AlertDialog';
 
 export const DeleteAllParticipantsButton = () => {
   const [showAlertDialog, setShowAlertDialog] = useState(false);
