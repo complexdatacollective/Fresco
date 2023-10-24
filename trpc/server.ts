8e20f3f9eae2e757fd7a57c27ef641f476278e9e
Joshua Melville <joshua.melville@northwestern.edu>
2023-10-24 11:30:40 +0200

    upgrade project dependencies

--- trpc/server.ts
+++ trpc/server.ts
@@ -1,9 +1,13 @@
 'use server';
 
-import { loggerLink } from '@trpc/client';
+import {
+  createTRPCProxyClient,
+  loggerLink,
+  unstable_httpBatchStreamLink,
+} from '@trpc/client';
 import { experimental_nextHttpLink } from '@trpc/next/app-dir/links/nextHttp';
 import { experimental_createTRPCNextAppDirServer } from '@trpc/next/app-dir/server';
-import { cookies } from 'next/headers';
+import { headers } from 'next/headers';
 import SuperJSON from 'superjson';
 import { env } from '~/env.mjs';
 import { type AppRouter } from '~/server/router';
@@ -37,13 +41,33 @@ export const api = experimental_createTRPCNextAppDirServer<AppRouter>({
         // }),
         experimental_nextHttpLink({
           batch: true,
-          headers: {
-            'cookie': cookies().toString(),
-            'x-trpc-source': 'rsc-invoke',
+          headers() {
+            const heads = new Map(headers());
+            heads.set('x-trpc-source', 'rsc-invoke');
+            return Object.fromEntries(heads);
           },
           url: getUrl(),
         }),
       ],
     };
   },
+});
+
+export const api2 = createTRPCProxyClient<AppRouter>({
+  transformer: SuperJSON,
+  links: [
+    loggerLink({
+      enabled: (op) =>
+        env.NODE_ENV === 'development' ||
+        (op.direction === 'down' && op.result instanceof Error),
+    }),
+    unstable_httpBatchStreamLink({
+      url: getUrl(),
+      headers() {
+        const heads = new Map(headers());
+        heads.set('x-trpc-source', 'rsc');
+        return Object.fromEntries(heads);
+      },
+    }),
+  ],
 });
