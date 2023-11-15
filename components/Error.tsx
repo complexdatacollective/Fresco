1056e0fa967cbb0656eb39d316bc1811ecf1a7aa
Caden Buckhalt <halaeb@gmail.com>
2023-11-15 14:29:45 -0800

    sendError func to wrap trackError

--- components/Error.tsx
+++ components/Error.tsx
@@ -3,8 +3,7 @@
 import { useEffect } from 'react';
 import { Button } from '~/components/ui/Button';
 import { AlertTriangle } from 'lucide-react';
-import { type ErrorPayload, trackError } from '@codaco/analytics';
-import { api } from '~/trpc/client';
+import { sendError } from '~/utils/sendError';
 
 export default function Error({
   error,
@@ -15,28 +14,11 @@ export default function Error({
   reset: () => void;
   heading?: string;
 }) {
-  const code = 123;
-  const stacktrace = 'stacktrace';
-  const appSettings = api.appSettings.get.useQuery();
-
   useEffect(() => {
-    // Log the error to an error reporting service
-    const errorPayload: ErrorPayload = {
-      code: code,
-      message: error.message,
-      details: heading ? heading : '',
-      stacktrace: stacktrace,
-      installationid: appSettings.data?.installationId ?? '',
-      path: '/interview',
-    };
-    trackError(errorPayload).catch((e) => {
-      // eslint-disable-next-line no-console
-      console.error('Error tracking error', e);
+    sendError({ error, heading }).catch((err) => {
+      console.error(err);
     });
-    // eslint-disable-next-line no-console
-    console.error(error);
-  }, [error, appSettings.data?.installationId, heading]);
-
+  }, [error, heading]);
   return (
     <div className="mx-auto my-4 flex max-w-md flex-col items-center rounded-lg border border-destructive p-4 text-center">
       <AlertTriangle className="mb-2 h-12 w-12 text-destructive" />
