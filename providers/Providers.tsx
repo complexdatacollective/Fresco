b6a9dcb1a422b9dda4e0b1c409d020d7676fedcf
Caden Buckhalt <halaeb@gmail.com>
2023-11-17 14:28:11 -0800

    rm plausible provider

--- providers/Providers.tsx
+++ providers/Providers.tsx
@@ -4,34 +4,18 @@ import { TRPCReactProvider } from '~/trpc/client';
 import { SessionProvider } from '~/providers/SessionProvider';
 import type { Session } from 'lucia';
 import { headers } from 'next/headers';
-import PlausibleProvider from 'next-plausible';
-import { env } from 'process';
-import { api } from '~/trpc/server';
 
-export default async function Providers({
+export default function Providers({
   children,
   initialSession,
 }: {
   children: React.ReactNode;
   initialSession: Session | null;
-}): Promise<ReactElement> {
-  const appSettings = await api.appSettings.get.query();
-
+}): ReactElement {
   return (
     <TRPCReactProvider headers={headers()}>
       <ReactQueryDevtools initialIsOpen={true} />
       <SessionProvider session={initialSession}>{children}</SessionProvider>
-      <PlausibleProvider
-        domain="fresco.networkcanvas.com"
-        taggedEvents={true}
-        manualPageviews={true}
-        trackLocalhost={env.NODE_ENV === 'development' ? true : false}
-        enabled={appSettings?.allowAnalytics}
-        // for production, add env.NODE_ENV === 'production' to disable tracking in dev
-        //enabled={appSettings?.allowAnalytics && env.NODE_ENV === 'production'}
-        selfHosted={true}
-        customDomain="https://analytics.networkcanvas.dev"
-      />
     </TRPCReactProvider>
   );
 }
