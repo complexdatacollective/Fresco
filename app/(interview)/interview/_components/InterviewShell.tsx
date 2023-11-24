27c0200d7a8965ec279068947dc1fb0acf131a99
Joshua Melville <joshua.melville@northwestern.edu>
2023-11-24 13:41:37 +0200

    remove redundant files and reimplement ProtocolScreen

--- app/(interview)/interview/_components/InterviewShell.tsx
+++ app/(interview)/interview/_components/InterviewShell.tsx
@@ -2,14 +2,23 @@
 
 import { motion } from 'framer-motion';
 import { Provider } from 'react-redux';
+import DialogManager from '~/lib/interviewer/components/DialogManager';
+import { SettingsMenu } from '~/lib/interviewer/components/SettingsMenu';
 import ProtocolScreen from '~/lib/interviewer/containers/ProtocolScreen';
-import { store } from '~/lib/interviewer/store';
+import configureAppStore from '~/lib/interviewer/store';
+import { useInterview } from '~/providers/InterviewProvider';
 
 const InterviewShell = () => {
+  const { protocol, network, currentStageIndex } = useInterview();
+
   return (
     <motion.div className="grid h-[100vh] grid-cols-2">
-      <Provider store={store}>
-        <ProtocolScreen stageIndex={0} />
+      <Provider
+        store={configureAppStore({ protocol, network, currentStageIndex })}
+      >
+        <ProtocolScreen />
+        <SettingsMenu />
+        <DialogManager />
       </Provider>
     </motion.div>
   );
