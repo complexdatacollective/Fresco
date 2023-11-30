4d1982a8bc8feff191d3ae82082a615c5383262e
Caden Buckhalt <halaeb@gmail.com>
2023-11-30 14:59:16 -0800

    wip create instance of analyticsclient

---
+++ utils/analytics.ts
@@ -0,0 +1,14 @@
+import { AnalyticsClient } from '@codaco/analytics';
+import { env } from '~/env.mjs';
+
+const globalForAnalytics = globalThis as unknown as {
+  analytics: AnalyticsClient | undefined;
+};
+
+export const analytics =
+  globalForAnalytics.analytics ??
+  new AnalyticsClient({
+    installationId: 'installationid',
+  });
+
+if (env.NODE_ENV !== 'production') globalForAnalytics.analytics = analytics;
