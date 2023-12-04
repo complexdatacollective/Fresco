51bbd00c1d9059ec2e13136b7ecc9857b73c6673
mrkarimoff <mrkarimoff99@gmail.com>
2023-12-04 19:17:18 +0500

    make header and sidebar separate components

--- app/dashboard-wireframe/layout.tsx
+++ app/dashboard-wireframe/layout.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import Sidebar from './_components/Sidebar';
+import Header from './_components/Header';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full bg-white">
       {/* Html should be used instead of div */}
-      <main className="h-full">{children}</main>
+      <main className="h-full">
+        <Header />
+        <Sidebar />
+        {children}
+      </main>
       {/* body should be used instead of main */}
     </div>
   );
