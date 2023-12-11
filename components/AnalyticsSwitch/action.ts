ecbc04a1e201c1b5541c1602ddc30b4bb1654ee7
Caden Buckhalt <halaeb@gmail.com>
2023-12-11 13:20:34 -0800

    update analyticsswitch components for persisting enabled

---
+++ components/AnalyticsSwitch/action.ts
@@ -0,0 +1,11 @@
+'use server';
+
+import { api } from '~/trpc/server';
+
+export async function setAnalytics(state: boolean) {
+  try {
+    await api.appSettings.updateAnalytics.mutate(state);
+  } catch (error) {
+    throw new Error(error as string);
+  }
+}
