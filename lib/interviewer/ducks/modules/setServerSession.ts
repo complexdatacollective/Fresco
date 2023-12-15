215b1f494afdf7ad9537d98a07e38198846e5362
mrkarimoff <mrkarimoff99@gmail.com>
2023-12-15 09:19:52 +0500

    fix: setServerSessin, ImportCSVModal, participants

--- lib/interviewer/ducks/modules/setServerSession.ts
+++ lib/interviewer/ducks/modules/setServerSession.ts
@@ -1,5 +1,5 @@
-import type { Protocol } from '@codaco/shared-consts';
-import { type Prisma } from '@prisma/client';
+import { type Protocol, type Prisma } from '@prisma/client';
+// import type { Protocol } from '@codaco/shared-consts';
 // import type { ServerSession } from '~/app/(interview)/interview/[interviewId]/page';
 
 // temporarily declaring this type
