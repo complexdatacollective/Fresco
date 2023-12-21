0b7675e8722de5cca999e3f572d157fc4bcaba97
Caden Buckhalt <halaeb@gmail.com>
2023-12-21 12:11:11 -0800

    style: nc icon color

--- app/(interview)/interview/finished/page.tsx
+++ app/(interview)/interview/finished/page.tsx
@@ -3,7 +3,7 @@ import { BadgeCheck } from 'lucide-react';
 export default function InterviewCompleted() {
   return (
     <div className="flex h-screen flex-col items-center justify-center bg-[var(--nc-background)]">
-      <BadgeCheck className="mb-4 h-12 w-12 text-white" />
+      <BadgeCheck className="mb-4 h-12 w-12 text-[var(--color-sea-green)]" />
       <h1 className="text-3xl font-extrabold text-white">
         Thank you for participating!
       </h1>
