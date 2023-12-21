79ae1a91b04c3ddf162f5fd5b06a9f28787df6fd
Caden Buckhalt <halaeb@gmail.com>
2023-12-21 12:10:45 -0800

    style: nc bg and icon colors

--- app/(interview)/interview/finished/page.tsx
+++ app/(interview)/interview/finished/page.tsx
@@ -2,12 +2,12 @@ import { BadgeCheck } from 'lucide-react';
 
 export default function InterviewCompleted() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center bg-gray-100">
-      <BadgeCheck className="mb-4 h-12 w-12 text-violet-600" />
-      <h1 className="text-3xl font-extrabold text-violet-700">
+    <div className="flex h-screen flex-col items-center justify-center bg-[var(--nc-background)]">
+      <BadgeCheck className="mb-4 h-12 w-12 text-white" />
+      <h1 className="text-3xl font-extrabold text-white">
         Thank you for participating!
       </h1>
-      <p className="text-lg text-gray-700">
+      <p className="text-lg text-white">
         Your interview has been successfully completed.
       </p>
     </div>
