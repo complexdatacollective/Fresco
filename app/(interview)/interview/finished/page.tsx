8788fbd02ec9b65bcd35016f494c6bf8648e8424
Caden Buckhalt <halaeb@gmail.com>
2023-12-21 11:28:15 -0800

    style: finished interview page

--- app/(interview)/interview/finished/page.tsx
+++ app/(interview)/interview/finished/page.tsx
@@ -1,7 +1,15 @@
-export default function Page() {
+import { BadgeCheck } from 'lucide-react';
+
+export default function InterviewCompleted() {
   return (
-    <div>
-      <h1>Thank you for participating!</h1>
+    <div className="flex h-screen flex-col items-center justify-center bg-gray-100">
+      <BadgeCheck className="mb-4 h-12 w-12 text-violet-600" />
+      <h1 className="text-3xl font-extrabold text-violet-700">
+        Thank you for participating!
+      </h1>
+      <p className="text-lg text-gray-700">
+        Your interview has been successfully completed.
+      </p>
     </div>
   );
 }
