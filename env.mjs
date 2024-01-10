ce82d093fe0f265961f4fe35259e4fb63ff60825
Joshua Melville <joshua.melville@northwestern.edu>
2024-01-10 15:21:29 +0200

    reenenable environment checking

--- env.mjs
+++ env.mjs
@@ -44,6 +44,5 @@ export const env = createEnv({
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
    * useful for Docker builds.
    */
-  // skipValidation: !!process.env.SKIP_ENV_VALIDATION,
-  skipValidation: true,
+  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
 });
