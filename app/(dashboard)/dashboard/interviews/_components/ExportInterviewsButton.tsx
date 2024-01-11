58d078b1d4565e643d3fc381982edafe792a9669
mrkarimoff <mrkarimoff99@gmail.com>
2024-01-11 20:00:52 +0500

    wip: trying to test functionality in Fresco

--- app/(dashboard)/dashboard/interviews/_components/ExportInterviewsButton.tsx
+++ app/(dashboard)/dashboard/interviews/_components/ExportInterviewsButton.tsx
@@ -1,7 +1,28 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable no-console */
 import { Button } from '~/components/ui/Button';
+import { exportToFile } from '~/lib/interviewer/utils/exportProcess';
 
 const ExportInterviewsButton = () => {
-  return <Button>Export all interviews</Button>;
+  function triggerExport() {
+    console.log('Export happens');
+
+    exportToFile(['interview 1', 'interview 2', 'interview 3'], 'filename')
+      .then(({ run, abort, setConsideringAbort }) => {
+        console.log({ abort, setConsideringAbort });
+
+        return run();
+      })
+      .then(() => console.log('Some action happens here'))
+      .catch((error: unknown) => {
+        console.log('error:', error);
+      });
+  }
+
+  return <Button onClick={triggerExport}>Export all interviews</Button>;
 };
 
 export default ExportInterviewsButton;
