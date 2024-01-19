a5d0c078181ccbab738e10f6ee99ea9a8b088ea6
Joshua Melville <joshua.melville@northwestern.edu>
2024-01-19 15:51:30 +0200

    add trackEvent to tRPC routes

--- app/api/trpc/[trpc]/route.ts
+++ app/api/trpc/[trpc]/route.ts
@@ -1,24 +1,34 @@
 import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
-import { type NextRequest } from 'next/server';
 import { env } from '~/env.mjs';
 import { appRouter } from '~/server/router';
 import { createTRPCContext } from '~/server/context';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { trackEvent } from '~/analytics/utils';
 
-const handler = (req: NextRequest) =>
+const handler = (req: NextApiRequest, res: NextApiResponse) =>
   fetchRequestHandler({
     endpoint: '/api/trpc',
     req,
     router: appRouter,
-    createContext: () => createTRPCContext({ req }),
-    onError:
-      env.NODE_ENV === 'development'
-        ? ({ path, error }) => {
-            // eslint-disable-next-line no-console
-            console.error(
-              `❌ tRPC failed on ${path ?? '<no-path>'}: ${error.message}`,
-            );
-          }
-        : undefined,
+    createContext: () => createTRPCContext({ req, res }),
+    onError(opts) {
+      const { error, type, path } = opts;
+
+      void trackEvent({
+        type: 'Error',
+        error: {
+          message: error.message,
+          details: type,
+          path: path ?? 'unknown',
+          stacktrace: 'unknown',
+        },
+      });
+
+      if (env.NODE_ENV === 'development') {
+        // eslint-disable-next-line no-console
+        console.error(error);
+      }
+    },
   });
 
 export { handler as GET, handler as POST };
