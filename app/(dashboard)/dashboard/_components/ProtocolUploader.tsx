56e0b570311f812f3550ceaf56ce16563ff89a2a
Joshua Melville <joshua.melville@northwestern.edu>
2024-02-09 17:37:12 +0200

    revert protocol uploader icon

--- app/(dashboard)/dashboard/_components/ProtocolUploader.tsx
+++ app/(dashboard)/dashboard/_components/ProtocolUploader.tsx
@@ -10,8 +10,6 @@ import usePortal from 'react-useportal';
 import { cn } from '~/utils/shadcn';
 import JobCard from '~/components/ProtocolImport/JobCard';
 import { withNoSSRWrapper } from '~/utils/NoSSRWrapper';
-import { ProtocolIcon } from './SummaryStatistics/Icons';
-import Image from 'next/image';
 
 function ProtocolUploader({
   handleProtocolUploaded,
@@ -58,14 +56,7 @@ function ProtocolUploader({
         {isActive ? (
           <Loader2 className="mr-2 inline-block h-4 w-4 animate-spin" />
         ) : (
-          // <FileUp className="mr-2 inline-block h-4 w-4" />
-          <Image
-            src="/images/protocol-icon.png"
-            alt="file-up"
-            width={30}
-            height={20}
-            className="mr-2 inline-block"
-          />
+          <FileUp className="mr-2 inline-block h-4 w-4" />
         )}
         <input {...getInputProps()} />
         Import protocols
