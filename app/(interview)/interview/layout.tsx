73ad2e7bb32f50dc9f25d9ae1f80f74d1b92e548
Caden Buckhalt <halaeb@gmail.com>
2024-03-29 12:31:45 -0700

    feat: overlay in interview layout for small screens

--- app/(interview)/interview/layout.tsx
+++ app/(interview)/interview/layout.tsx
@@ -5,9 +5,23 @@ export const metadata = {
   description: 'Interview',
 };
 
+function SmallScreenOverlay() {
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center p-8 backdrop-blur-sm sm:hidden">
+      <div className=" text-2xl font-semibold text-primary-foreground">
+        <p>
+          To view this content, please maximize the window or try again on a
+          device with a larger screen.
+        </p>
+      </div>
+    </div>
+  );
+}
+
 function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <main className="flex h-[100vh] max-h-[100vh] flex-col bg-[--nc-background] text-[--nc-text]">
+      <SmallScreenOverlay />
       {children}
     </main>
   );
