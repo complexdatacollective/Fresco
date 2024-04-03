39a0afbe76d0d2b6f902a85149e9e9826ebca08d
Caden Buckhalt <halaeb@gmail.com>
2024-04-03 11:45:44 -0700

    clean up styles

--- app/(interview)/interview/_components/SmallScreenOverlay.tsx
+++ app/(interview)/interview/_components/SmallScreenOverlay.tsx
@@ -4,15 +4,13 @@ import { AlertTriangle } from 'lucide-react';
 
 export const SmallScreenOverlay = () => {
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-[--nc-background] p-8 sm:hidden">
-      <div className="bg-gray-100 flex h-screen flex-col items-center justify-center">
-        <AlertTriangle className="text-violet-700 mb-4 h-12 w-12" />
-        <Heading variant="h1">Screen Size Too Small</Heading>
-        <Paragraph variant="lead">
-          To view this content, please maximize the window or try again on a
-          device with a larger screen.
-        </Paragraph>
-      </div>
+    <div className="fixed inset-0 z-50 flex flex-col items-center justify-center bg-[--nc-background] p-8 sm:hidden">
+      <AlertTriangle className="mb-4 h-12 w-12" />
+      <Heading variant="h1">Screen Size Too Small</Heading>
+      <Paragraph variant="lead">
+        To view this content, please maximize the window or try again on a
+        device with a larger screen.
+      </Paragraph>
     </div>
   );
 };
