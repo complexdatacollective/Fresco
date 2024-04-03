be980ca77f349fee9789da8b37d55a9f4b1b8787
Caden Buckhalt <halaeb@gmail.com>
2024-04-03 11:33:48 -0700

    fix: breakpoint at small screens

--- app/(interview)/interview/_components/SmallScreenOverlay.tsx
+++ app/(interview)/interview/_components/SmallScreenOverlay.tsx
@@ -4,7 +4,7 @@ import { AlertTriangle } from 'lucide-react';
 
 export const SmallScreenOverlay = () => {
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-[--nc-background] p-8 md:hidden">
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-[--nc-background] p-8 sm:hidden">
       <div className="bg-gray-100 flex h-screen flex-col items-center justify-center">
         <AlertTriangle className="text-violet-700 mb-4 h-12 w-12" />
         <Heading variant="h1">Screen Size Too Small</Heading>
