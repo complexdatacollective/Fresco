b0a8c9e46d643c5248483fa78af4285bec220633
Joshua Melville <joshua.melville@northwestern.edu>
2024-04-22 15:54:06 +0200

    revert zod and react-hook-form version bumps

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -49,8 +49,8 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
             type={type}
             className={cn(
               'focus-visible:ring-ring flex h-10 w-full rounded-input border border-border bg-input px-3 py-2 text-sm text-input-foreground ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
-              leftAdornment && 'pl-10',
-              rightAdornment && 'pr-10',
+              !!leftAdornment && 'pl-10',
+              !!rightAdornment && 'pr-10',
               inputClassName,
             )}
             ref={ref}
