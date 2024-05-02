bc7573a19ba87f25914aaf7933fb86bbe2039dda
Joshua Melville <joshua.melville@northwestern.edu>
2024-05-02 15:18:00 +0200

    reenable check for analytics

--- lib/analytics.ts
+++ lib/analytics.ts
@@ -2,5 +2,5 @@ import { makeEventTracker } from '@codaco/analytics';
 import { env } from '~/env.mjs';
 
 export const trackEvent = makeEventTracker({
-  enabled: true,
+  enabled: !env.NEXT_PUBLIC_DISABLE_ANALYTICS,
 });
