6439f226aafcf629a65e4f09374f34275b63bb54
Joshua Melville <joshua.melville@northwestern.edu>
2024-05-02 15:43:19 +0200

    update react-resize-aware to fix error with boolean option component

--- components/ui/typography/Paragraph.tsx
+++ components/ui/typography/Paragraph.tsx
@@ -4,30 +4,27 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { forwardRef } from 'react';
 import { cn } from '~/utils/shadcn';
 
-export const paragraphVariants = cva(
-  'text-foreground text-pretty font-normal',
-  {
-    variants: {
-      variant: {
-        default: '',
-        blockquote: 'mt-6 border-l-2 pl-6 italic',
-        inlineCode:
-          'relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold',
-        lead: 'mb-3 text-lg text-opacity-70 font-semibold md:text-xl leading-6',
-        mutedText: 'text-muted',
-        smallText: 'text-sm',
-      },
-      margin: {
-        default: '[&:not(:first-child)]:mt-4',
-        none: 'mt-0',
-      },
+export const paragraphVariants = cva('text-pretty font-normal', {
+  variants: {
+    variant: {
+      default: '',
+      blockquote: 'mt-6 border-l-2 pl-6 italic',
+      inlineCode:
+        'relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold',
+      lead: 'mb-3 text-lg text-opacity-70 font-semibold md:text-xl leading-6',
+      mutedText: 'text-muted',
+      smallText: 'text-sm',
     },
-    defaultVariants: {
-      variant: 'default',
-      margin: 'default',
+    margin: {
+      default: '[&:not(:first-child)]:mt-4',
+      none: 'mt-0',
     },
   },
-);
+  defaultVariants: {
+    variant: 'default',
+    margin: 'default',
+  },
+});
 
 export type ParagraphProps = {
   variant?: VariantProps<typeof paragraphVariants>['variant'];
