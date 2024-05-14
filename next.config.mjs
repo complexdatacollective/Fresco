29e275dd9cff8e8a0c140706b270261b472b685f
Caden Buckhalt <halaeb@gmail.com>
2024-05-14 07:15:35 -0700

    rm node-rs packages added for lucia v3
    not needed in v2

--- next.config.mjs
+++ next.config.mjs
@@ -7,8 +7,7 @@ import pkg from './package.json' with { type: 'json' };
 import ChildProcess from 'child_process';
 
 // starts a command line process to get the git hash
-const commitHash = ChildProcess
-  .execSync('git log --pretty=format:"%h" -n1')
+const commitHash = ChildProcess.execSync('git log --pretty=format:"%h" -n1')
   .toString()
   .trim();
 
@@ -25,19 +24,16 @@ const config = {
       test: /\.(jpe?g|png|svg|gif|ico|eot|ttf|woff|woff2|mp4|pdf|webm|txt|mp3)$/,
       type: 'asset/resource',
       generator: {
-        filename: 'static/chunks/[path][name].[hash][ext]'
+        filename: 'static/chunks/[path][name].[hash][ext]',
       },
     });
-
-    // add the node-rs externals to the webpack config so they arent bundled: https://lucia-auth.com/getting-started/nextjs-app
-    config.externals.push("@node-rs/argon2", "@node-rs/bcrypt");
 
     return config;
   },
   env: {
     // add the package.json version and git hash to the environment
     APP_VERSION: pkg.version,
-    COMMIT_HASH: commitHash
-  }
+    COMMIT_HASH: commitHash,
+  },
 };
 export default config;
