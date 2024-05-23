b002a6f5dd2466ae8a3c35bb491f433c135e64e8
Joshua Melville <joshua.melville@northwestern.edu>
2024-05-23 20:43:29 +0200

    add k6 load test example

--- utils/getBaseUrl.ts
+++ utils/getBaseUrl.ts
@@ -1,10 +1,6 @@
 import { env } from '~/env.mjs';
 
 export default function getBaseUrl() {
-  if (typeof window !== 'undefined')
-    // browser should use relative path
-    return `${window.location.protocol}//${window.location.host}`;
-
   if (env.VERCEL_URL)
     // reference for vercel.com
     return `https://${env.VERCEL_URL}`;
@@ -14,5 +10,6 @@ export default function getBaseUrl() {
     return env.NEXT_PUBLIC_URL;
 
   // assume localhost
+  console.log('caught');
   return `http://127.0.0.1:3000`;
 }
