e10b7a63ea51ccc57ce5577401171d70962f1201
Mirfayz Karimoff <mrkarimoff99@gmail.com>
2024-05-30 17:16:16 +0500

    fix refetching behavior by using clearOndefault option in nuqs

--- app/dashboard/_components/ActivityFeed/ActivityFeed.tsx
+++ app/dashboard/_components/ActivityFeed/ActivityFeed.tsx
@@ -2,24 +2,16 @@
 
 import type { Events } from '@prisma/client';
 import { useQuery } from '@tanstack/react-query';
-import { hash } from 'ohash';
+import { useSearchParams } from 'next/navigation';
 import { DataTableSkeleton } from '~/components/data-table/data-table-skeleton';
 import ActivityFeedTable from './ActivityFeedTable';
-import { useTableStateFromSearchParams } from './useTableStateFromSearchParams';
 
 export const ActivityFeed = () => {
-  const { searchParams } = useTableStateFromSearchParams();
+  const params = useSearchParams();
 
   const { isPending, data } = useQuery({
-    queryKey: ['activityFeed', hash(searchParams)],
+    queryKey: ['activityFeed', params.toString()],
     queryFn: async () => {
-      const params = new URLSearchParams();
-      params.set('page', searchParams.page.toString());
-      params.set('perPage', searchParams.perPage.toString());
-      params.set('sort', searchParams.sort);
-      params.set('sortField', searchParams.sortField);
-      params.set('filterParams', JSON.stringify(searchParams.filterParams));
-
       const response = await fetch(`/api/activity-feed?${params.toString()}`, {
         next: { tags: ['activityFeed'] },
       });
