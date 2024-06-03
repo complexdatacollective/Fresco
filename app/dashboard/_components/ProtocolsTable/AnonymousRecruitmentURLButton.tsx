dca060c44252b3ca64b4e91a0c841b3a2ddc780b
Joshua Melville <joshua.melville@northwestern.edu>
2024-06-03 13:00:38 +0200

    handle window not defined in recruitment url button

--- app/dashboard/_components/ProtocolsTable/AnonymousRecruitmentURLButton.tsx
+++ app/dashboard/_components/ProtocolsTable/AnonymousRecruitmentURLButton.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { Check, Copy } from 'lucide-react';
+import { useEffect, useState } from 'react';
 import { Button } from '~/components/ui/Button';
 import { useToast } from '~/components/ui/use-toast';
 
@@ -10,8 +11,19 @@ export const AnonymousRecruitmentURLButton = ({
   protocolId: string;
 }) => {
   const { toast } = useToast();
-  const url = `${window.location.origin}/onboard/${protocolId}`;
+  const [url, setUrl] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setUrl(`${window.location.origin}/onboard/${protocolId}`);
+    }
+  }, [protocolId]);
+
   const handleCopyClick = () => {
+    if (!url) {
+      return;
+    }
+
     navigator.clipboard
       .writeText(url)
       .then(() => {
