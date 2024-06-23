0bcdff65d92c03ffe06c8c15629a5eb0f60ea241
Joshua Melville <joshua.melville@northwestern.edu>
2024-06-23 06:50:49 +0100

    add meta tags to interview layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -16,6 +16,16 @@ const Layout = async ({ children }: { children: React.ReactNode }) => {
 
   return (
     <>
+      <meta
+        name="viewport"
+        content="width=device-width; initial-scale=1; viewport-fit=cover"
+      />
+      <meta name="mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta
+        name="apple-mobile-web-app-status-bar-style"
+        content="black-translucent"
+      />
       <NavigationBar />
       <FeedbackBanner />
       {children}
