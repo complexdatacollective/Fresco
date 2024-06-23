a0ed7fb2edef0a8b1ab2b0488e1f1247cd6ba772
Joshua Melville <joshua.melville@northwestern.edu>
2024-06-23 06:40:40 +0100

    initial PWA setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,50 @@
+import { Viewport, type Metadata } from 'next';
+import { Quicksand } from 'next/font/google';
 import { Toaster } from '~/components/ui/toaster';
 import '~/styles/globals.scss';
-import { Quicksand } from 'next/font/google';
 
-export const metadata = {
-  title: 'Network Canvas Fresco',
-  description: 'Fresco.',
+const APP_NAME = 'Fresco';
+const APP_DEFAULT_TITLE = 'Network Canvas Fresco';
+const APP_TITLE_TEMPLATE = '%s - Fresco';
+const APP_DESCRIPTION = 'Network Canvas interviews in the web browser.';
+
+export const metadata: Metadata = {
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'default',
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: 'website',
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: 'summary',
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: '#FFFFFF',
 };
 
 const quicksand = Quicksand({
