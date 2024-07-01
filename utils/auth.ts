a6ba7c0a57f4594ab8981eb82669569b163a4905
Mirfayz Karimoff <mrkarimoff99@gmail.com>
2024-07-01 18:59:06 +0500

    update utils/auth.ts to migrate to lucia V3

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,47 +1,100 @@
-import { prisma as prismaAdapter } from '@lucia-auth/adapter-prisma';
-import type { User } from '@prisma/client';
-import { lucia } from 'lucia';
-import { nextjs_future } from 'lucia/middleware';
+/* eslint-disable @typescript-eslint/consistent-type-definitions */
+import { PrismaAdapter } from '@lucia-auth/adapter-prisma';
+import { type User } from '@prisma/client';
+import { Lucia } from 'lucia';
 import 'lucia/polyfill/node'; // polyfill for Node.js versions <= 18
-import * as context from 'next/headers';
-import { RedirectType, redirect } from 'next/navigation';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 import { cache } from 'react';
 import 'server-only';
 import { env } from '~/env';
-import { prisma as client } from '~/utils/db';
+import { prisma } from '~/utils/db';
 
-export const auth = lucia({
-  env: env.NODE_ENV === 'production' ? 'PROD' : 'DEV',
-  middleware: nextjs_future(),
-  sessionCookie: {
-    expires: false,
-  },
+const adapter = new PrismaAdapter(prisma.session, prisma.user);
+
+export const auth = new Lucia(adapter, {
   getUserAttributes: (data: User) => {
     return {
       username: data.username,
     };
   },
-  adapter: prismaAdapter(client),
+  sessionCookie: {
+    name: 'fresco-session',
+    // this sets cookies with super long expiration
+    // since Next.js doesn't allow Lucia to extend cookie expiration when rendering pages
+    expires: false,
+    attributes: {
+      // set to `true` when using HTTPS
+      secure: env.NODE_ENV === 'production',
+    },
+  },
 });
 
-export const getServerSession = cache(() => {
-  const authRequest = auth.handleRequest('GET', context);
-  return authRequest.validate();
+declare module 'lucia' {
+  interface Register {
+    Lucia: typeof auth;
+    // DatabaseSessionAttributes: DatabaseSessionAttributes;
+    DatabaseUserAttributes: DatabaseUserAttributes;
+  }
+}
+
+interface DatabaseUserAttributes {
+  id: string;
+  username: string;
+}
+
+export const getServerSession = cache(async () => {
+  const sessionId = cookies().get(auth.sessionCookieName)?.value ?? null;
+  if (!sessionId)
+    return {
+      session: null,
+      user: null,
+    };
+  const result = await auth.validateSession(sessionId);
+  try {
+    if (result.session?.fresh) {
+      const sessionCookie = auth.createSessionCookie(result.session.id);
+      cookies().set(
+        sessionCookie.name,
+        sessionCookie.value,
+        sessionCookie.attributes,
+      );
+    }
+    if (!result.session) {
+      const sessionCookie = auth.createBlankSessionCookie();
+      cookies().set(
+        sessionCookie.name,
+        sessionCookie.value,
+        sessionCookie.attributes,
+      );
+    }
+  } catch {
+    // Next.js throws error when attempting to set cookies when rendering page
+  }
+  return result;
 });
 
 export type Auth = typeof auth;
 
-export async function requirePageAuth() {
-  const session = await getServerSession();
+export async function requirePageAuth({
+  redirectPath,
+}: {
+  redirectPath?: string | null;
+} = {}) {
+  const { session } = await getServerSession();
 
   if (!session) {
-    redirect('/signin', RedirectType.replace);
+    if (!redirectPath) {
+      redirect('/signin');
+    }
+
+    redirect('/signin?callbackUrl=' + encodeURIComponent(redirectPath));
   }
   return session;
 }
 
 export async function requireApiAuth() {
-  const session = await getServerSession();
+  const { session } = await getServerSession();
 
   if (!session) {
     throw new Error('Unauthorized');
