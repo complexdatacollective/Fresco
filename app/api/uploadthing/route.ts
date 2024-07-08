8b2dadd5046c2d4b203bb7fe3aaf2d26053963bd
Mirfayz Karimoff <mrkarimoff99@gmail.com>
2024-07-08 14:18:39 +0500

    fix protocol upload feature by setting Uploadthing callback Url for docker deployment

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -1,7 +1,14 @@
 import { createRouteHandler } from 'uploadthing/next';
+import { env } from '~/env';
 import { ourFileRouter } from './core';
 
 // Export routes for Next App Router
 export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
+  config: {
+    callbackUrl:
+      env.NODE_ENV === 'production' && env.PUBLIC_URL
+        ? env.PUBLIC_URL + '/api/uploadthing'
+        : undefined,
+  },
 });
