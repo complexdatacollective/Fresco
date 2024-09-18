f6097f01385bc46e8ee567105e201f5ed04da476
Joshua Melville <joshua.melville@northwestern.edu>
2024-09-18 12:55:31 +0200

    don't import typescript in next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 import('./env.js');
 import ChildProcess from 'node:child_process';
 import pkg from './package.json' with { type: 'json' };
-import { ensureError } from './utils/ensureError';
-
 
 let commitHash = 'Unknown commit hash';
 
@@ -10,11 +8,9 @@ try {
   commitHash = ChildProcess.execSync('git log --pretty=format:"%h" -n1')
     .toString()
     .trim()
-} catch (e) {
-
-  const error = ensureError(e);
+} catch (error) {
   // eslint-disable-next-line no-console
-  console.info('Error getting commit hash:', error.message);
+  console.info('Error getting commit hash:', error.message ?? 'Unknown error');
 }
 
 /** @type {import("next").NextConfig} */
