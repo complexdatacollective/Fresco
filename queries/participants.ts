2524aefe42e47cd9bd65443aa5940fe97eb4e7af
Joshua Melville <joshua.melville@northwestern.edu>
2024-09-20 10:48:14 +0200

    sort participants table by most recently added

--- queries/participants.ts
+++ queries/participants.ts
@@ -8,6 +8,8 @@ export const getParticipants = createCachedFunction(async () => {
       interviews: true,
       _count: { select: { interviews: true } },
     },
+    // Sort to show the most recently created first
+    orderBy: { id: 'desc' },
   });
 
   return participants;
