71465506061634cbebd8e35239fa290c4d1a850b
Joshua Melville <joshua.melville@northwestern.edu>
2024-10-16 15:59:28 +0200

    migrate data entirely in SQL

--- actions/reset.ts
+++ actions/reset.ts
@@ -1,13 +1,16 @@
 'use server';
 
 import { revalidatePath } from 'next/cache';
+import { env } from 'process';
 import { UTApi } from 'uploadthing/server';
 import { safeRevalidateTag } from '~/lib/cache';
 import { requireApiAuth } from '~/utils/auth';
 import { prisma } from '~/utils/db';
 
 export const resetAppSettings = async () => {
-  await requireApiAuth();
+  if (env.NODE_ENV !== 'development') {
+    await requireApiAuth();
+  }
 
   try {
     // Delete all data:
