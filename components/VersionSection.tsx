cef8b647945742e931f365a9cfc19862d3dc6431
Caden Buckhalt <halaeb@gmail.com>
2024-10-24 16:43:48 -0500

    first pass at semver update messaging

--- components/VersionSection.tsx
+++ components/VersionSection.tsx
@@ -3,6 +3,7 @@
 import { CheckCircle2, Info, Loader2, XCircle } from 'lucide-react';
 import { use, useEffect, useState } from 'react';
 import { z } from 'zod';
+import { Alert, AlertDescription, AlertTitle } from '~/components/ui/Alert';
 import { env } from '~/env';
 import { type getInstallationId } from '~/queries/appSettings';
 import { ensureError } from '~/utils/ensureError';
@@ -13,7 +14,35 @@ const GithubApiResponseSchema = z.object({
   status: z.string(),
   ahead_by: z.number(),
   behind_by: z.number(),
+  base_commit: z.object({
+    sha: z.string(),
+  }),
 });
+
+const GithubApiTagSchema = z.array(
+  z.object({
+    commit: z.object({
+      sha: z.string(),
+    }),
+    name: z.string(),
+  }),
+);
+
+function getSemverUpdateType(
+  currentVersion: string,
+  mainVersion: string,
+): 'major' | 'minor' | 'patch' | 'same' {
+  if (currentVersion === mainVersion) return 'same';
+
+  const [currentMajor, currentMinor, currentPatch] = currentVersion.split('.');
+  const [mainMajor, mainMinor, mainPatch] = mainVersion.split('.');
+
+  if (mainMajor !== currentMajor) return 'major';
+  if (mainMinor !== currentMinor) return 'minor';
+  if (mainPatch !== currentPatch) return 'patch';
+
+  return 'same';
+}
 
 export default function VersionSection({
   installationIdPromise,
@@ -28,32 +57,45 @@ export default function VersionSection({
     upToDate: boolean;
     aheadBy: number;
     behindBy: number;
+    semVerUpdateType: 'major' | 'minor' | 'patch' | 'same';
   } | null>(null);
 
   useEffect(() => {
     setIsLoading(true);
-    fetch(
-      `https://api.github.com/repos/complexdatacollective/fresco/compare/${env.COMMIT_HASH}...main`,
-    )
-      .then((res) => res.json())
-      .then(
-        (result: unknown) => {
-          setIsLoading(false);
 
-          const response = GithubApiResponseSchema.parse(result);
-          setData({
-            upToDate:
-              response.status === 'identical' || response.status === 'behind',
-            aheadBy: response.ahead_by,
-            behindBy: response.behind_by,
-          });
-        },
-        (error) => {
-          const e = ensureError(error);
-          setIsLoading(false);
-          setError(e.message);
-        },
-      );
+    Promise.all([
+      fetch(
+        `https://api.github.com/repos/complexdatacollective/fresco/compare/${env.COMMIT_HASH}...main`,
+      )
+        .then((res) => res.json())
+        .then((result) => GithubApiResponseSchema.parse(result)),
+      fetch(`https://api.github.com/repos/complexdatacollective/fresco/tags`)
+        .then((res) => res.json())
+        .then((result) => GithubApiTagSchema.parse(result)),
+    ])
+      .then(([compareResponse, tags]) => {
+        const mainTag = tags.find(
+          (tag) => tag.commit.sha === compareResponse.base_commit.sha,
+        );
+
+        const mainVersion = mainTag.name.replace(/^v/, '');
+
+        setData({
+          upToDate:
+            compareResponse.status === 'identical' ||
+            compareResponse.status === 'behind',
+          aheadBy: compareResponse.ahead_by,
+          behindBy: compareResponse.behind_by,
+          semVerUpdateType: getSemverUpdateType(mainVersion, env.APP_VERSION),
+        });
+      })
+      .catch((error) => {
+        const e = ensureError(error);
+        setError(e.message);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   }, []);
 
   return (
@@ -111,6 +153,46 @@ export default function VersionSection({
         ).
       </Paragraph>
       <Paragraph>Your unique installation ID is: {installationID}</Paragraph>
+      {data?.semVerUpdateType === 'major' && <MajorVersionAlert />}
+      {data?.semVerUpdateType === 'minor' && <MinorVersionAlert />}
+      {data?.semVerUpdateType === 'patch' && <PatchVersionAlert />}
     </SettingsSection>
   );
 }
+
+const MajorVersionAlert = () => {
+  return (
+    <Alert variant="destructive" className="mt-4">
+      <AlertTitle>Warning:</AlertTitle>
+      <AlertDescription>
+        A major version bump will require additional configuration work, and
+        should NOT be done while collecting data. If you are actively collecting
+        data, please wait until data collection is complete before updating.
+      </AlertDescription>
+    </Alert>
+  );
+};
+
+const MinorVersionAlert = () => {
+  return (
+    <Alert variant="info" className="mt-4">
+      <AlertTitle>Note:</AlertTitle>
+      <AlertDescription>
+        A minor version bump will add new features. Please review the release
+        notes and update.
+      </AlertDescription>
+    </Alert>
+  );
+};
+
+const PatchVersionAlert = () => {
+  return (
+    <Alert variant="info" className="mt-4">
+      <AlertTitle>Note:</AlertTitle>
+      <AlertDescription>
+        A patch version bump will include bug fixes. We recommend updating to
+        all patch versions.
+      </AlertDescription>
+    </Alert>
+  );
+};
