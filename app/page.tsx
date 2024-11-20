02e35fae9cbb93dceb667973b4637426524e663d
Joshua Melville <joshua.melville@northwestern.edu>
2024-11-20 14:58:12 +0200

    make root redirect permanent

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import { redirect } from 'next/navigation';
+import { permanentRedirect } from 'next/navigation';
 
 export default function Home() {
-  redirect('/dashboard');
+  permanentRedirect('/dashboard');
 }
