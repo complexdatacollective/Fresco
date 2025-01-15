10edc6e90e11490c43ccff15409458a5095fd675
Joshua Melville <joshua.melville@northwestern.edu>
2025-01-15 11:29:34 +0200

    migrate to ts config

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
-import('./env.js');
+import { type NextConfig } from 'next';
 import ChildProcess from 'node:child_process';
+import './env';
 import pkg from './package.json' with { type: 'json' };
 
 let commitHash = 'Unknown commit hash';
@@ -9,28 +10,21 @@ try {
     .toString()
     .trim();
 } catch (error) {
-  // eslint-disable-next-line no-console
-  console.info('Error getting commit hash:', error.message ?? 'Unknown error');
+  if (error instanceof Error) {
+    // eslint-disable-next-line no-console
+    console.info(
+      'Error getting commit hash:',
+      error.message ?? 'Unknown error',
+    );
+  }
 }
 
 /** @type {import("next").NextConfig} */
-const config = {
+const config: NextConfig = {
   output: 'standalone',
   reactStrictMode: true,
   experimental: {
     typedRoutes: true,
-    webpackBuildWorker: true,
-  },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(jpe?g|png|svg|gif|ico|eot|ttf|woff|woff2|mp4|pdf|webm|txt|mp3)$/,
-      type: 'asset/resource',
-      generator: {
-        filename: 'static/chunks/[path][name].[hash][ext]',
-      },
-    });
-
-    return config;
   },
   env: {
     // add the package.json version and git hash to the environment
