d785ae25a38d6a41b626d788199b4a5d94a9eb7d
Joshua Melville <joshua.melville@northwestern.edu>
2025-01-20 14:03:29 +0200

    resolve issue with state resetting

--- app/(interview)/interview/_components/InterviewShell.tsx
+++ app/(interview)/interview/_components/InterviewShell.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import { parseAsInteger, useQueryState } from 'nuqs';
-import { useState } from 'react';
 import { Provider } from 'react-redux';
 import DialogManager from '~/lib/interviewer/components/DialogManager';
 import ProtocolScreen from '~/lib/interviewer/containers/ProtocolScreen';
@@ -12,9 +10,6 @@ import ServerSync from './ServerSync';
 // and create a redux store with that data.
 // Eventually it will handle syncing this data back.
 const InterviewShell = ({ serverPayload }) => {
-  const [initialized, setInitialized] = useState(false);
-  const [currentStage, setCurrentStage] = useQueryState('step', parseAsInteger);
-
   return (
     <Provider store={store(serverPayload)}>
       <ServerSync />
