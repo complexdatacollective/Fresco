7000120d2055bb92e610cbef7c0cea8164c149aa
buckhalt <buckhalt@northwestern.edu>
2025-03-05 14:16:42 -0800

    upade playwright test command, ignore playwright in vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,5 +6,6 @@ export default defineConfig({
   plugins: [tsconfigPaths(), react()],
   test: {
     environment: 'jsdom',
+    exclude: ['**/e2e/**', '**/node_modules/**'], // e2e tests are handled by playwright
   },
 });
