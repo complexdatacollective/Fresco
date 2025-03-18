e5c1e04979c973469ebca95aebe74cd1e21011e6
buckhalt <buckhalt@northwestern.edu>
2025-03-18 14:38:10 -0700

    add basic testing of participants page

--- e2e/app.spec.ts
+++ e2e/app.spec.ts
@@ -1,9 +1,7 @@
-
 import { expect } from '@playwright/test';
 import { test } from './fixtures/signIn';
 
-test('should visit protocols page ', async ({ page }) => {
-
+test('should navigate to protocols page ', async ({ page }) => {
   await page.goto("/");
   await expect(page).toHaveURL(/\/dashboard/, { timeout: 10000 });
 
@@ -18,3 +16,17 @@ test('should visit protocols page ', async ({ page }) => {
   await expect(page).toHaveScreenshot('protocols-page.png');
 });
 
+test('should navigate to participants page ', async ({ page }) => {
+  await page.goto("/");
+  await expect(page).toHaveURL(/\/dashboard/, { timeout: 10000 });
+  // click "participants" in the top navigation
+  await page.click('text=Participants');
+  await expect(page).toHaveURL(/\/dashboard\/participants/, { timeout: 10000 });
+
+  // check that our imported participants are visible
+  await expect(page.locator('text=Christopher Lee')).toHaveText('Christopher Lee');
+  await expect(page.locator('text=Emily Brown')).toHaveText('Emily Brown');
+
+  // validate screenshot
+  await expect(page).toHaveScreenshot('participants-page.png');
+});
