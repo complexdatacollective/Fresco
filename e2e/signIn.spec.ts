9c297664d8b201d4e6a2fc3dfae1b1e898edb81c
buckhalt <buckhalt@northwestern.edu>
2025-03-19 11:11:19 -0700

    reset storage state in signIn test to avoid auth
    also fix uploadthing env var

---
+++ e2e/signIn.spec.ts
@@ -0,0 +1,20 @@
+import { expect, test } from '@playwright/test';
+
+// Reset storage state for this file to avoid being authenticated
+// allows us to test the sign in page
+test.use({ storageState: { cookies: [], origins: [] } });
+test('should navigate to signin page ', async ({ page }) => {
+  await page.goto("/");
+  await expect(page).toHaveURL(/\/signin/, { timeout: 10000 });
+});
+
+test('should sign in and navigate to dashboard ', async ({ page }) => {
+  await page.goto("/");
+  await expect(page).toHaveURL(/\/signin/, { timeout: 10000 });
+
+  await page.fill('input[name="username"]', 'admin');
+  await page.fill('input[name="password"]', 'Administrator1!');
+  await page.click('button[type="submit"]');
+
+  await expect(page).toHaveURL(/\/dashboard/, { timeout: 10000 });
+});
