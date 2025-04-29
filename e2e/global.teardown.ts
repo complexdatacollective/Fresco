3f645ce6db20850c37fe1bb4ff53b2c1acf9aecc
buckhalt <buckhalt@northwestern.edu>
2025-04-29 14:44:35 -0700

    refactor global teardown with regular non project dependency approach

--- e2e/global.teardown.ts
+++ e2e/global.teardown.ts
@@ -1,10 +1,10 @@
 /* eslint-disable no-console */
 /* eslint-disable no-process-env */
-import { test as teardown } from '@playwright/test';
+import { chromium, type FullConfig } from '@playwright/test';
 import { execSync } from 'child_process';
 import { UTApi } from 'uploadthing/server';
 
-teardown('delete test database', async ({ playwright, baseURL }) => {
+export default async function globalTeardown(config: FullConfig) {
   if (!process.env.CI) {
     // remove uploaded files from uploadthing
     // eslint-disable-next-line no-console
@@ -25,17 +25,17 @@ teardown('delete test database', async ({ playwright, baseURL }) => {
       stdio: 'inherit',
     });
   } else {
+    const { baseURL } = config.projects[1]?.use ?? {};
+
+    const browser = await chromium.launch();
+    const page = await browser.newPage();
+
     console.log('🚮 Clearing cache');
-    const requestContext = await playwright.request.newContext({
-      baseURL,
-    });
     try {
-      await requestContext.get('/reset');
+      await page.goto(baseURL!);
       console.log('✅ Application cache cleared successfully');
     } catch (error) {
       console.error('Failed to clear cache:', error);
     }
-
-    await requestContext.dispose();
   }
-});
+}
