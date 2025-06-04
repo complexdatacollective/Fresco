a949fdc935bbccbb49074d950ef80ba833e7250a
buckhalt <buckhalt@northwestern.edu>
2025-06-04 13:27:27 -0700

    implement scroll to first error

--- lib/interviewer/containers/TanStackForms/Form.tsx
+++ lib/interviewer/containers/TanStackForms/Form.tsx
@@ -1,14 +1,95 @@
 import {
   type ComponentType,
+  type ValidationError,
   type VariableType,
 } from '@codaco/protocol-validation';
 import { type VariableValue } from '@codaco/shared-consts';
-import { useForm } from '@tanstack/react-form';
+import { useForm, type ValidationErrorMap } from '@tanstack/react-form';
 import { useMemo } from 'react';
 import { useSelector } from 'react-redux';
 import { makeRehydrateFields } from '../../selectors/forms';
 import type { VariableValidation } from '../../utils/field-validation';
 import Field from './Field';
+
+const getScrollParent = (node: HTMLElement): Element => {
+  const regex = /(auto|scroll)/;
+  const parents = (_node: Element, ps: Element[]): Element[] => {
+    if (_node.parentNode === null) {
+      return ps;
+    }
+    return parents(_node.parentNode as Element, ps.concat([_node]));
+  };
+
+  const style = (_node: Element, prop: string): string =>
+    getComputedStyle(_node, null).getPropertyValue(prop);
+
+  const overflow = (_node: Element): string =>
+    style(_node, 'overflow') +
+    style(_node, 'overflow-y') +
+    style(_node, 'overflow-x');
+
+  const scroll = (_node: Element): boolean => regex.test(overflow(_node));
+
+  const scrollParent = (_node: Element): Element => {
+    if (!(_node instanceof HTMLElement || _node instanceof SVGElement)) {
+      return document.scrollingElement ?? document.documentElement;
+    }
+
+    const ps = parents(_node.parentNode as Element, []);
+
+    for (const p of ps) {
+      if (scroll(p)) {
+        return p;
+      }
+    }
+
+    return document.scrollingElement ?? document.documentElement;
+  };
+
+  return scrollParent(node);
+};
+
+const scrollToFirstError = (
+  errors: Record<
+    string,
+    {
+      errors: ValidationError[];
+      errorMap: ValidationErrorMap;
+    }
+  >,
+) => {
+  // Todo: first item is an assumption that may not be valid. Should iterate and check
+  // vertical position to ensure it is actually the "first" in page order (topmost).
+  if (!errors) {
+    return;
+  }
+
+  const firstError = Object.keys(errors)[0];
+
+  // All Fields have a name corresponding to variable ID so look this up.
+  // When used on alter form, multiple forms can be differentiated by the active slide
+  // class. This needs priority, so look it up first.
+  const el: HTMLElement | null =
+    document.querySelector(`.swiper-slide-active [name="${firstError}"]`) ??
+    document.querySelector(`[name="${firstError}"]`);
+
+  // If element is not found, prevent crash.
+  if (!el) {
+    // eslint-disable-next-line no-console
+    console.warn(
+      `scrollToFirstError(): Element [name="${firstError}"] not found in DOM`,
+    );
+    return;
+  }
+
+  // Subtract 200 to put more of the input in view.
+  const topPos = el.offsetTop - 200;
+
+  // Assume forms are inside a scrollable
+  const scroller = getScrollParent(el);
+
+  scroller.scrollTop = topPos;
+};
 
 type Field = {
   prompt: string;
@@ -64,6 +145,10 @@ const TanStackForm = ({
     onSubmit: ({ value }) => {
       // will only be called if validation passes
       handleFormSubmit(value);
+    },
+    onSubmitInvalid: ({ formApi }) => {
+      const errors = formApi.getAllErrors().fields;
+      scrollToFirstError(errors);
     },
   });
 
