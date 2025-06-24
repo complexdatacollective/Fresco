c22308fdab88fd1ea11ee8aa59e86aaffe00d6e9
Joshua Melville <joshua.melville@northwestern.edu>
2025-06-24 17:59:59 +0200

    correct migration

--- prisma/migrations/20250624154707_add_disable_small_screen_overlay/migration.sql
+++ prisma/migrations/20250624154707_add_disable_small_screen_overlay/migration.sql
@@ -1,8 +1,2 @@
 -- AlterEnum
 ALTER TYPE "AppSetting" ADD VALUE 'disableSmallScreenOverlay';
-
--- AlterTable
-ALTER TABLE "_AssetToProtocol" ADD CONSTRAINT "_AssetToProtocol_AB_pkey" PRIMARY KEY ("A", "B");
-
--- DropIndex
-DROP INDEX "_AssetToProtocol_AB_unique";
