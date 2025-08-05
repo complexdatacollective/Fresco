1a6dbe31d520488c5780a852363fe2fd48dc9f9f
Eric Carty-Fickes <ericcf@northwestern.edu>
2025-08-05 09:02:02 -0500

    updated layout algo and capturing node network data

---
+++ lib/ui/components/FamilyTree/ChildConnector.tsx
@@ -0,0 +1,54 @@
+import classNames from 'classnames';
+import { forwardRef } from 'react';
+
+/**
+ * Renders a connector between children and their parents' offspring connector.
+ */
+
+const FamilyTreeChildConnector = forwardRef<
+  HTMLDivElement,
+  {
+    xStartPos: number;
+    xEndPos: number;
+    yPos: number;
+    height: number;
+    color?: string;
+  }
+>((props, ref) => {
+  const {
+    xStartPos = 0,
+    xEndPos = 0,
+    yPos = 0,
+    height = 0,
+    color = '#666',
+  } = props;
+  const classes = classNames('child-connector');
+  const width = xEndPos - xStartPos;
+
+  return (
+    <div className={classes} ref={ref}>
+      <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
+        <line
+          x1={xStartPos}
+          y1={yPos}
+          x2={xStartPos}
+          y2={yPos + height}
+          stroke={color}
+          strokeLinecap="round"
+        />
+        <line
+          x1={xStartPos}
+          y1={yPos}
+          x2={xEndPos}
+          y2={yPos}
+          stroke={color}
+          strokeLinecap="round"
+        />
+      </svg>
+    </div>
+  );
+});
+
+FamilyTreeChildConnector.displayName = 'FamilyTreeChildConnector';
+
+export default FamilyTreeChildConnector;
--- lib/ui/components/FamilyTree/PartnerConnector.tsx
+++ lib/ui/components/FamilyTree/PartnerConnector.tsx
@@ -5,18 +5,20 @@ import { forwardRef } from 'react';
  * Renders a connector between partners.
  */
 
-const FamilyTreePartnerConnector = forwardRef<HTMLDivElement, { xStartPos: number, xEndPos: number, yPos: number }>((props, ref) => {
-  const {
-    xStartPos = 0,
-    xEndPos = 0,
-    yPos = 0,
-  } = props;
+const FamilyTreePartnerConnector = forwardRef<
+  HTMLDivElement,
+  { xStartPos?: number; xEndPos?: number; yPos?: number }
+>((props, ref) => {
+  const { xStartPos = 0, xEndPos = 0, yPos = 0 } = props;
   const classes = classNames('partner-connector');
+
+  if (xStartPos == 0 && xEndPos == 0 && yPos == 0) {
+    return;
+  }
 
   return (
     <div className={classes} ref={ref}>
       <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
-
         <line x1={xStartPos} y1={yPos} x2={xEndPos} y2={yPos} stroke="#666" />
       </svg>
     </div>
