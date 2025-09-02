9ada180dcbd907ef64d246d032b1200296d8d51b
buckhalt <buckhalt@northwestern.edu>
2025-09-02 08:43:15 -0700

    type fixes related to new protocol-validaiton package version

--- schemas/protocol.ts
+++ schemas/protocol.ts
@@ -1,4 +1,4 @@
-import { ProtocolSchema } from '@codaco/protocol-validation';
+import { VersionedProtocolSchema } from '@codaco/protocol-validation';
 import { z } from 'zod';
 
 const assetInsertSchema = z.object({
@@ -13,10 +13,9 @@ const assetInsertSchema = z.object({
 
 export type AssetInsertType = z.infer<typeof assetInsertSchema>;
 
-export const protocolInsertSchema = z
-  .object({
-    protocol: ProtocolSchema,
-    protocolName: z.string(),
-    newAssets: z.array(assetInsertSchema),
-    existingAssetIds: z.array(z.string()),
-  });
+export const protocolInsertSchema = z.object({
+  protocol: VersionedProtocolSchema,
+  protocolName: z.string(),
+  newAssets: z.array(assetInsertSchema),
+  existingAssetIds: z.array(z.string()),
+});
