ab56c0bf3f0253848c6cd03923b23011a0951e72
Ian Sieg <ian.sieg@northwestern.edu>
2025-11-03 11:37:00 -0600

    return null again

--- utils/db.ts
+++ utils/db.ts
@@ -74,7 +74,7 @@ const createPrismaClient = () =>
               schemaVersion,
               stages,
               codebook: {}, // dummy data
-              experiments: {},
+              experiments: null,
             });
             return protocolSchema.stages;
           },
@@ -92,7 +92,7 @@ const createPrismaClient = () =>
               schemaVersion,
               stages: [],
               codebook,
-              experiments: {},
+              experiments: null,
             });
             return protocolSchema.codebook;
           },
@@ -113,7 +113,7 @@ const createPrismaClient = () =>
               codebook: {},
               experiments,
             });
-            return protocolSchema.experiments ?? {};
+            return protocolSchema.experiments ?? null;
           },
         },
       },
