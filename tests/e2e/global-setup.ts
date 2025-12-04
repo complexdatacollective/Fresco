fe16c2c189eb4a5009cce3100dd3c06d5c3abb09
Joshua Melville <joshua.melville@northwestern.edu>
2025-12-04 21:06:27 +0200

    lint and fix workflow

--- tests/e2e/global-setup.ts
+++ tests/e2e/global-setup.ts
@@ -1,7 +1,7 @@
-import { exec, execFile } from 'child_process';
-import * as path from 'path';
-import { fileURLToPath } from 'url';
-import { promisify } from 'util';
+import { execFile } from 'node:child_process';
+import * as path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { promisify } from 'node:util';
 import { ADMIN_CREDENTIALS } from './config/test-config';
 import {
   saveContextData,
@@ -15,7 +15,6 @@ import {
 import { logger } from './utils/logger';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const execAsync = promisify(exec);
 const execFileAsync = promisify(execFile);
 
 async function globalSetup() {
@@ -195,9 +194,12 @@ async function ensureDockerImage() {
   try {
     await execFileAsync('docker', [
       'build',
-      '--build-arg', 'DISABLE_IMAGE_OPTIMIZATION=true',
-      '-t', imageName,
-      '-f', dockerfile,
+      '--build-arg',
+      'DISABLE_IMAGE_OPTIMIZATION=true',
+      '-t',
+      imageName,
+      '-f',
+      dockerfile,
       projectRoot,
     ]);
 
