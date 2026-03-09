e872986894e47e31be8ae5adfdf736adb6d4527c
Joshua Melville <joshua.melville@northwestern.edu>
2026-03-09 15:43:54 +0200

    chore(family-tree): remove unused exports and dead code

--- lib/interviewer/Interfaces/FamilyTreeCensus/utils/nodeUtils.ts
+++ lib/interviewer/Interfaces/FamilyTreeCensus/utils/nodeUtils.ts
@@ -19,27 +19,3 @@ export const getNodeSexVariable = createSelector(getCurrentStage, (stage) => {
 
   return stage.nodeSexVariable;
 });
-
-export const getRelationshipToEgoVariable = createSelector(
-  getCurrentStage,
-  (stage) => {
-    invariant(
-      stage.type === 'FamilyTreeCensus',
-      'Stage must be FamilyTreeCensus',
-    );
-
-    return stage.relationshipToEgoVariable;
-  },
-);
-
-export const getNodeIsEgoVariable = createSelector(getCurrentStage, (stage) => {
-  invariant(
-    stage.type === 'FamilyTreeCensus',
-    'Stage must be FamilyTreeCensus',
-  );
-
-  return stage.nodeIsEgoVariable;
-});
-
-export const normalizeRelationshipToEgoLabel = (str: string): string =>
-  str.toLowerCase().replace(/'/g, '').replace(/\s+/g, '-');
