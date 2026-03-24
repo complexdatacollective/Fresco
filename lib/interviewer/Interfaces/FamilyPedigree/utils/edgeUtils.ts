a5861f839c9022f5982eb6750af0e1a32ddf7e39
Joshua Melville <joshua.melville@northwestern.edu>
2026-03-24 11:35:24 +0200

    refactor: update FamilyPedigree selectors for new stage config shape

--- lib/interviewer/Interfaces/FamilyPedigree/utils/edgeUtils.ts
+++ lib/interviewer/Interfaces/FamilyPedigree/utils/edgeUtils.ts
@@ -2,11 +2,21 @@ import { createSelector } from '@reduxjs/toolkit';
 import { invariant } from 'es-toolkit';
 import { getCurrentStage } from '~/lib/interviewer/selectors/session';
 
-export const getRelationshipTypeVariable = createSelector(
-  getCurrentStage,
-  (stage) => {
-    invariant(stage.type === 'FamilyPedigree', 'Stage must be FamilyPedigree');
+export const getEdgeConfig = createSelector(getCurrentStage, (stage) => {
+  invariant(stage.type === 'FamilyPedigree', 'Stage must be FamilyPedigree');
+  return stage.edgeConfig;
+});
 
-    return stage.edgeConfig.relationshipTypeVariable;
-  },
+export const getEdgeType = createSelector(getEdgeConfig, (c) => c.type);
+export const getRelationshipTypeVariable = createSelector(
+  getEdgeConfig,
+  (c) => c.relationshipTypeVariable,
+);
+export const getIsActiveVariable = createSelector(
+  getEdgeConfig,
+  (c) => c.isActiveVariable,
+);
+export const getIsGestationalCarrierVariable = createSelector(
+  getEdgeConfig,
+  (c) => c.isGestationalCarrierVariable,
 );
--- lib/interviewer/Interfaces/FamilyPedigree/utils/nodeUtils.ts
+++ lib/interviewer/Interfaces/FamilyPedigree/utils/nodeUtils.ts
@@ -2,14 +2,26 @@ import { createSelector } from '@reduxjs/toolkit';
 import { invariant } from 'es-toolkit';
 import { getCurrentStage } from '~/lib/interviewer/selectors/session';
 
-export const getEgoShapeVariable = createSelector(getCurrentStage, (stage) => {
+export const getNodeConfig = createSelector(getCurrentStage, (stage) => {
   invariant(stage.type === 'FamilyPedigree', 'Stage must be FamilyPedigree');
-
-  return stage.egoShapeVariable;
+  return stage.nodeConfig;
 });
 
-export const getNodeShapeVariable = createSelector(getCurrentStage, (stage) => {
-  invariant(stage.type === 'FamilyPedigree', 'Stage must be FamilyPedigree');
-
-  return stage.nodeConfig.shapeVariable;
-});
+export const getNodeType = createSelector(getNodeConfig, (c) => c.type);
+export const getNodeLabelVariable = createSelector(
+  getNodeConfig,
+  (c) => c.nodeLabelVariable,
+);
+export const getEgoVariable = createSelector(
+  getNodeConfig,
+  (c) => c.egoVariable,
+);
+export const getBiologicalSexVariable = createSelector(
+  getNodeConfig,
+  (c) => c.biologicalSexVariable,
+);
+export const getRelationshipVariable = createSelector(
+  getNodeConfig,
+  (c) => c.relationshipVariable,
+);
+export const getNodeForm = createSelector(getNodeConfig, (c) => c.form);
